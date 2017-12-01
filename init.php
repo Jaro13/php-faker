333cbd4849e0f7d135787c235c2433b44bdf066a
Your Name <you@example.com>
2017-12-01 15:15:25 +0000

    start-2

--- init.php
+++ init.php
@@ -1,7 +1,11 @@
 <?php
 
 
-require 'vendor/autoload.php';
+
+//ta opcja jest chyba jepsza
+require __DIR__ . '/vendor/autoload.php';
+//require 'vendor/autoload.php';
+
 
 $connection = mysqli_connect("127.0.0.1", "root", "password", "faker1");
 
